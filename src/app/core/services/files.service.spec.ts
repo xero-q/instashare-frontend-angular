ad994d929ec71901aff471f367fcce06b3d137ee
Anibal Sanchez <anibal.sanchez@dspot.com.pl>
2024-09-16 10:16:21 -0400

    Added integration tests for FilesService

--- src/app/core/services/files.service.spec.ts
+++ src/app/core/services/files.service.spec.ts
@@ -1,9 +1,11 @@
 import { TestBed } from '@angular/core/testing';
 import { FilesService } from './files.service';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClientTestingModule,HttpTestingController } from '@angular/common/http/testing';
+import { environment } from '../../../environments/environment';
 
 describe('FilesService', () => {
   let service: FilesService;
+  let httpTestingController:HttpTestingController;
 
   beforeEach(() => {    
     TestBed.configureTestingModule({
@@ -11,9 +13,50 @@ describe('FilesService', () => {
       providers: [FilesService]
     });    
     service = TestBed.inject(FilesService);
+    httpTestingController = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpTestingController.verify(); 
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should fetch files with pagination', () => {
+    const mockResponse = {
+      results: [
+        { id: 1, name: 'File 1' },
+        { id: 2, name: 'File 2' }
+      ],
+      count: 2
+    };
+
+    service.getFiles(1, 20).subscribe(files => {
+      expect(files.results.length).toBe(2);
+      expect(files.count).toBe(2);
+      expect(files.results[0].name).toBe('File 1');
+    });
+
+    const req = httpTestingController.expectOne(`${environment.API_URL}/api/files?page=1&perPage=20`);
+    expect(req.request.method).toBe('GET');
+    req.flush(mockResponse);
+  });
+
+  it('should update a file name', () => {
+    const mockResponse = { success: true };
+
+    const updatedFileName = 'Updated File';
+    const fileId = 1;
+    
+    service.updateFile(fileId, updatedFileName).subscribe(response => {
+      expect(response.success).toBe(true);
+    });
+
+    const req = httpTestingController.expectOne(`${environment.API_URL}/api/upload/${fileId}`);
+    expect(req.request.method).toBe('PUT');
+    expect(req.request.body).toEqual({ name: updatedFileName });   
+    req.flush(mockResponse);
   });
 });
